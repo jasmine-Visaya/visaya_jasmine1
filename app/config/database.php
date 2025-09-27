a09be4ebaebeac16c6e64a3c7fead164acfdb824
Jasmine <visayajasminegrace2004@gmail.com>
2025-09-27 17:21:52 +0800

    commit

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12799356',
-    'password'	=> '7g2BvqVHTW',
-    'database'	=> 'sql12799356',
+    'username'	=> 'sql12800289',
+    'password'	=> 'vr9ScALi6C',
+    'database'	=> 'sql12800289',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
